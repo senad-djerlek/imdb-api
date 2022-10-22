c79da8c662f0c2a94d413a1df6fed79e9bdf183f
Senad Djerlek <senajerlek@gmail.com>
2022-10-22 23:26:28 +0200

    fixing bugs for deploy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
+
 import "./App.css";
 import NavBar from "./components/NavBar";
-import { Pagination } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-import Movies from "./components/Movies";
+
 import Footer from "./components/Footer";
 import MoviePage from "./pages/MoviePage";
-import TvShows from "./components/TvShows";
+
 import MoviesInfo from "./components/MoviesInfo";
 const App = () => {
   return (
     <>
       <div className="App">
         <NavBar />
-        <div className="page-content" style={{ height: "86vh" }}>
+        <div className="page-content" style={{ height: "100%" }}>
           <Routes>
             <Route path="/" element={<MoviePage />} />
             <Route
